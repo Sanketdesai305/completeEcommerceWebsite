f15c63a5f70aaa8629679a2f428b348f4c467ce2
Sanketdesai305 <desai.sanket92@gmail.com>
2023-01-30 17:12:27 +0530

    Update responsive.js

--- src/responsive.js
+++ src/responsive.js
@@ -7,3 +7,19 @@ export const mobile = (props) => {
     }
   `;
 };
+
+export const mobile1 = (props) => {
+  return css`
+    @media only screen and (max-width: 420px) {
+      ${props}
+    }
+  `;
+};
+
+export const screen1 = (props) => {
+  return css`
+    @media only screen and (max-width: 1024px) {
+      ${props}
+    }
+  `;
+};
